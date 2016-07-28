7f0de9c1d982ac029d801a4fe9b824e0044ac259
cmdrtorefresh <cmdrtorefresh@gmail.com>
2016-07-27 23:36:59 -0700

    Contact Me page mobile version done

---
+++ js/biojquery.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+  $('menuitem').mouseenter(function(){
+    $(this).animate({
+      opacity: "+=0.5"
+    },200);
+  }).mouseleave(function(){
+    $(this).animate({
+      opacity: "-=0.5"
+    },200);
+  });
+
+  $('resumelink').mouseenter(function(){
+    $(this).animate({
+      opacity: "+=0.5"
+    },200);
+  }).mouseleave(function(){
+    $(this).animate({
+      opacity: "-=0.5"
+    },200);
+  });
+
+  $('menulogo').click(function(){
+    if($('#dropdownmenu').css('display') == 'none'){
+      $(this).css("background-image", "url(../images/exit.png)");
+    } else {
+      $(this).css("background-image", "url(../images/menu.png)");
+    }
+    $('#dropdownmenu').slideToggle('slow');
+  });
+
+});
